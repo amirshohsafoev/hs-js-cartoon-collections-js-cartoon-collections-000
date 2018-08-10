f368b327fcbd87ef852f3a288c66c4b25e0b1771
Amir Safoev <amirsafoev0@gmail.com>
2018-08-10 21:11:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function dwarfRollCall(dwarves) {
+  var string = "The dwarves are numbered: "
+  for (var i = 0; i < dwarves.length - 1 ; i++  ){
+
+     string += `${i + 1}. ${dwarves[i]}, `
+    }
+    
+    string += `${dwarves.length}. ${dwarves[dwarves.length-1]}`
+    
+    return string;
+  
 }
 
 function summonCaptainPlanet(planeteerCalls){
