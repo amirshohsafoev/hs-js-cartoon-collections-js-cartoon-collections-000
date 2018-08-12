c81c63c9f64f1d4824900abae18c35b0de8a8802
Amir Safoev <amirsafoev0@gmail.com>
2018-08-12 15:46:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 function dwarfRollCall(dwarves) {
+
   var string = ""
   for (var i = 0; i < dwarves.length; i++){
 
      string += `${i + 1}. ${dwarves[i]} `
-   
-  } 
-    return string;
+   }
+   return string;
   
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let i = 0;
+  while (i < planeteerCalls.length){
+    planeteerCalls[i] += "!";
+   // planeteerCalls.toUpperCase();
+     i++;
+  }
+return  planeteerCalls
+ 
 }
 
+
 function longPlaneteerCalls(words) {
+  var i = 0;
+  if ( words[i].length < 4){
+    return false
+    i++;
+}else {
+  return true
+}
 }
 
 function findTheCheese (foods) {
