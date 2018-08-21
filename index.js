5dae125874e49d0def57eb5ab9751092321f7f62
Amir Safoev <amirsafoev0@gmail.com>
2018-08-21 20:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function dwarfRollCall(dwarves) {
 
   var string = ""
-  for (var i = 0; i + dwarves.length/2 < dwarves.length; i++){
+  for (var i = 0; i < dwarves.length; i++){
 
      string += `${i + 1}. ${dwarves[i]} `
 
