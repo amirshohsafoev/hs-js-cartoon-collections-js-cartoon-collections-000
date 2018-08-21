1a5199a612dcc7412925bb433ce14127db8ba50c
Amir Safoev <amirsafoev0@gmail.com>
2018-08-21 20:08:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
-  var cheese = "cheddar, gouda, camembert";
+  var cheese = "cheddar, gouda, camembert, anothercheese";
   for ( var i = 0; i < foods.length; i++) {
     if  (cheese.indexOf(foods[i]) !== -1){
         return foods[i];
